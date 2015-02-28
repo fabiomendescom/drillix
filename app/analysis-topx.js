a43c7edc640447879ce4c9f3e6d6ade85861c862
Fabio Mendes <fabio@fmendes.net>
2015-02-27 23:53:38 -0800

    improved the graph programming

--- app/analysis-topx.js
+++ app/analysis-topx.js
@@ -1,18 +1,90 @@
 var data = {
+	"defaults" : {
+		"scale" : "1",
+		"boxheight" : "100",
+		"boxwidth" : "600",
+		"leftmargin" : "20",
+		"topmargin" : "40",
+		"boxspacing": "25"
+	},
 	"title": "Top 5",
 	"type": "TOPX",
 	"mode": "single",
+	"parameters" : [
+		{"rankingproperty" : "Product"},
+		{"rankingmeasure" : "Sales"}
+	],
 	"base": { 
+		"dataslice" : {
+			"datacontext": "Context",
+			"timefrom" : "2010-01-01",
+			"timeto" : "2010-01-31",
+			"properties" : [
+				{
+					"property" : {
+						"name" : "Region",
+						"type" : ""
+					}
+				},
+				{
+					"property" : {
+						"name" : "State",
+						"type" : ""
+					}					
+				}
+			],
+			"measures" : [
+				{
+					"measure" : {
+						"name" : "value",
+						"type" : ""
+					}
+				}
+			]
+		},
 		"data":[
-			{"label": "Product A", "value": 545, "accumvalue": 1000, "perc": .10, "accumperc": .20},
-			{"label": "Product B", "value": 200, "accumvalue": 1000, "perc": .10, "accumperc": .20},
-			{"label": "Product C", "value": 150, "accumvalue": 1000, "perc": .10, "accumperc": .20},
-			{"label": "Product D", "value": 150, "accumvalue": 1000, "perc": .10, "accumperc": .20},
-			{"label": "Product E", "value": 120, "accumvalue": 1000, "perc": .10, "accumperc": .20}
+			{
+				"label": "Product A", 
+				"value": 545,
+				"comments": [
+					{
+						"from": "Fabio Mendes",
+						"image": "1.jpg",
+						"date" : "Jan 3, 10:00pm",
+						"comment": "This is a comment"
+					}
+				],
+				"alarms" : [
+					{
+						"image": "Alarm.png"
+					}
+				]
+			},
+			{
+				"label": "Product B", 
+				"value": 200,
+				"comments" : []
+			},
+			{
+				"label": "Product C", 
+				"value": 150,
+				"comments" : []
+			},
+			{
+				"label": "Product D", 
+				"value": 150,
+				"comments" : []
+			},
+			{
+				"label": "Product E", 
+				"value": 120,
+				"comments" : []
+			}
 		],
 		"other": {
+			"label": "Other",
 			"value": 1232,
-			"label": "Other"
+			"comments" : []
 		}
 	},
 	"comparison" : {
@@ -22,47 +94,108 @@ var data = {
 
 drillix = {};
 drillix.analysis = {};
+drillix.layout = {};	
 
-drillix.analysis.topx = function module() {
+drillix.layout.topx = function(dt) {
+	var tmpdata = dt;
+	
+	var scale = parseInt(dt.defaults.scale);	
+	var boxheight = parseInt(dt.defaults.boxheight);
+	var boxwidth = parseInt(dt.defaults.boxwidth);
+	var box_x = parseInt(dt.defaults.leftmargin);
+	var box_y = parseInt(dt.defaults.topmargin);	
+	var boxspacing = parseInt(dt.defaults.boxspacing);
 
-	var scale = 1;
-	
-	var boxheight=100;
-	var boxwidth=600;
-	var box_x = 20;
-	var box_y = 40;
-	
-	var boxspacing = 25;
-		
-	function exports(_selection) {	
-		_selection.each(function(_data) {
-			//sizes
-			boxheight = boxheight*scale;
-			boxwidth = boxwidth*scale;
-			numberranksize = boxheight*.3;
-			numberfontsize = numberranksize*1.20;	
-			boxspacing = boxspacing * scale;	
-			labelfontsize = numberfontsize*.50;
+	//sizes
+	boxheight = boxheight*scale;
+	boxwidth = boxwidth*scale;
+	numberranksize = boxheight*.3;
+	numberfontsize = numberranksize*1.20;	
+	boxspacing = boxspacing * scale;	
+	labelfontsize = numberfontsize*.50;
 							
-			//positions	
-			start_x = (box_x + numberranksize);
-			start_y = (box_y + numberranksize);		
-			circlestart_x = box_x + (numberranksize*1.50);
-			circlestart_y = box_y + (numberranksize*1.50);
-			numberstart_x = box_x + (numberranksize*1.10);
-			numberstart_y = box_y + (numberranksize*1.80);
+	//positions	
+	start_x = (box_x + numberranksize);
+	start_y = (box_y + numberranksize);		
+	circlestart_x = box_x + (numberranksize*1.50);
+	circlestart_y = box_y + (numberranksize*1.50);
+	numberstart_x = box_x + (numberranksize*1.10);
+	numberstart_y = box_y + (numberranksize*1.80);
 
-			//spacing
-			labelxspacing = numberstart_x + (numberranksize*1.6);
-			underlineoffsety = numberfontsize / 2.77;
-			underlinesize = boxwidth * 0.96;
-			barmargin = numberranksize * 1.4;
-			baroffset = underlineoffsety * 3.2;
-			barheight = numberranksize * .5;
-			accumnumberoffset = baroffset * .85;
-			valuesfontsize = labelfontsize * .5;
-			numberoffset = accumnumberoffset + barheight + (valuesfontsize * 2);
-						
+	//spacing
+	labelxspacing = numberstart_x + (numberranksize*1.6);
+	underlineoffsety = numberfontsize / 2.77;
+	underlinesize = boxwidth * 0.96;
+	barmargin = numberranksize * 1.4;
+	baroffset = underlineoffsety * 3.2;
+	barheight = numberranksize * .5;
+	accumnumberoffset = baroffset * .85;
+	valuesfontsize = labelfontsize * .5;
+	numberoffset = accumnumberoffset + barheight + (valuesfontsize * 2);
+	
+	measurebarmaxwidth = boxwidth * .95;
+	measurebarratio = measurebarmaxwidth / tmpdata.base.data[0].value;
+	
+	i=0;
+	while (i < tmpdata.base.data.length) {
+		tmpdata.base.data[i].x = start_x;
+		tmpdata.base.data[i].y = start_y+(i*boxheight)+(i*boxspacing);	
+		tmpdata.base.data[i].width = boxwidth;
+		tmpdata.base.data[i].height = boxheight;	
+		tmpdata.base.data[i].ranking_x = circlestart_x;
+		tmpdata.base.data[i].ranking_y = circlestart_y+(i*boxheight)+(i*boxspacing);
+		tmpdata.base.data[i].rankingcirclesize = numberranksize;
+		tmpdata.base.data[i].rankingfontsize = numberranksize;
+		tmpdata.base.data[i].rankingnumber_x = numberstart_x;		
+		tmpdata.base.data[i].rankingnumber_y = numberstart_y+(i*boxheight)+(i*boxspacing);
+		tmpdata.base.data[i].rankingnumber = i+1;
+		tmpdata.base.data[i].labelfontsize = labelfontsize;
+		tmpdata.base.data[i].rankingtext_y = numberstart_y+(i*boxheight)+(i*boxspacing);
+		tmpdata.base.data[i].rankingtext_x = labelxspacing;
+		tmpdata.base.data[i].textunderline_y = numberstart_y+(i*boxheight)+(i*boxspacing) + underlineoffsety;
+		tmpdata.base.data[i].textunderline_start = labelxspacing;
+		tmpdata.base.data[i].textunderline_end = numberstart_x+underlinesize;
+		tmpdata.base.data[i].measurebar_x = box_x+barmargin;
+		tmpdata.base.data[i].measurebar_y = numberstart_y+(i*boxheight)+(i*boxspacing) + baroffset;
+		tmpdata.base.data[i].measurebar_width = tmpdata.base.data[i].value * measurebarratio;
+		tmpdata.base.data[i].measurebar_height = barheight;
+		tmpdata.base.data[i].measurevaluefontsize = valuesfontsize;
+		tmpdata.base.data[i].measurevalue_x = box_x+barmargin;
+		tmpdata.base.data[i].measurevalue_y = numberstart_y+(i*boxheight)+(i*boxspacing) + accumnumberoffset;
+		
+		j=0;
+		while (j < tmpdata.base.data[i].comments.length) {
+			tmpdata.base.data[i].comments[j].box_x = 300;
+			tmpdata.base.data[i].comments[j].box_y = 1;
+			tmpdata.base.data[i].comments[j].box_height = 35;
+			tmpdata.base.data[i].comments[j].box_width = 140;
+									
+			tmpdata.base.data[i].comments[j].from_x = 340;
+			tmpdata.base.data[i].comments[j].from_y = 15;
+			tmpdata.base.data[i].comments[j].from_height = 33;
+			tmpdata.base.data[i].comments[j].from_width = 140;
+
+			tmpdata.base.data[i].comments[j].date_x = 340;
+			tmpdata.base.data[i].comments[j].date_y = 32;
+			tmpdata.base.data[i].comments[j].date_height = 33;
+			tmpdata.base.data[i].comments[j].date_width = 140;
+			
+			tmpdata.base.data[i].comments[j].image_x = 301;
+			tmpdata.base.data[i].comments[j].image_y = 2;		
+			
+			j++;
+		}
+		i++;
+	}
+	
+    return tmpdata;
+ };
+
+drillix.analysis.topx = function module(dt) {
+		
+	function exports(_selection)	 {	
+		_selection.each(function(_data) {
+
 			if (!svg) {
 				//title
 				var title = d3.select(this).append("h1")
@@ -73,7 +206,7 @@ drillix.analysis.topx = function module() {
 					.attr("style","width:16px;height:32px;margin-right:5px");
 					
 				title.append("span")
-					.text("Top 5")		
+					.text(_data.title)		
 					
 				//title box
 				var commenttitlelabel = d3.select(this).append("div").attr("style","margin-left:20px;font-weight:bold").text("Title");
@@ -163,8 +296,7 @@ drillix.analysis.topx = function module() {
 					
 				var property2 = filtertbl
 					.append("tr");
-					
-					
+									
 				var propertytext2 = property2.append("td")
 					.attr("colspan","2")
 					.attr("style","border:1px solid grey;padding:5px")
@@ -274,7 +406,62 @@ drillix.analysis.topx = function module() {
 				.attr('x',491)
 				.attr('y',2)				
 				.attr('width',32)
-				.attr('height',32);			
+				.attr('height',32);
+				
+				var datapointline = svg.append('g');
+				datapointline.append('line')
+			    .attr('stroke', 'grey')
+				.attr('y1',133)
+				.attr('x1',370)
+				.attr('y2',35)
+				.attr('x2',370)
+			
+				var datapoint = svg.append('g')
+				datapoint.append('circle')
+				.attr('cy', 143)
+				.attr('cx', 370) 
+				.attr('r', 10)
+				.attr('stroke', 'red')
+				.attr('fill', 'white')	
+				datapoint.append('circle')
+				.attr('cy', 143)
+				.attr('cx',370)
+				.attr('r', 7) 
+				.attr('stroke', 'red')
+				.attr('fill', 'white')	
+				datapoint.append('circle')
+				.attr('cy', 143)
+				.attr('cx',370)
+				.attr('r', 4) 
+				.attr('stroke', 'red')
+				.attr('fill', 'white')	
+				datapoint.append('circle')
+				.attr('cy', 143)
+				.attr('cx',370)
+				.attr('r', 1) 
+				.attr('stroke', 'red')
+				.attr('fill', 'white')	
+				datapoint.append('line')
+				.attr('y1',143)
+				.attr('x1',360)
+				.attr('y2',143)
+				.attr('x2',380)
+				.attr('stroke', 'black') 
+				datapoint.append('line')
+				.attr('y1',133)
+				.attr('x1',370)
+				.attr('y2',153)
+				.attr('x2',370)
+				.attr('stroke', 'grey')
+				 
+				var alarmline = svg.append('g');
+				alarmline.append('line')
+			    .attr('stroke', 'grey')
+				.attr('y1',133)
+				.attr('x1',370)
+				.attr('y2',34)
+				.attr('x2',510)														
+							
 								
 				var container = svg.append("g").classed("container",true);
 				container.append("g").classed("rankingbargroup",true);
@@ -282,240 +469,143 @@ drillix.analysis.topx = function module() {
 			
 			var rankingbars = svg.select(".container").select('.rankingbargroup').selectAll('.bar').data(_data.base.data);
 			
-			drawboxes(rankingbars,_data);	
-			    
+			// Draw boxes    	
+
+			el = rankingbars.enter()
+				.append('g');
+		
+			//ranking box
+			el.append("rect")
+				.classed('bar',true)
+				.classed('shadow',true)
+					.attr('fill','#ffffc0')
+					.attr('opacity',.5)
+					.attr('stroke','grey')
+					.style('stroke-width',1)				
+					.attr('x',function(d,i) {
+						return d.x;
+					})
+					.attr('y',function(d,i) {
+						return d.y;
+					})				
+					.attr('width',function(d,i) {
+						return d.width;
+					})
+					.attr('height',function(d,i) {
+						return d.height;
+					});		
+		
+			//ranking number circles
+			el.append("circle").classed('bar',true)
+				.classed('shadow',true)
+					.attr('fill','#ffffc0')
+					.attr('stroke','grey')
+					.style('stroke-width',1)
+					.transition()
+					.attr('cx', function(d,i) {
+						return d.ranking_x;
+					})
+					.attr('cy',function(d,i) {
+						return d.ranking_y;
+					})
+					.attr('r',function(d,i) {
+						return d.rankingcirclesize;
+					})
+					.attr('height',function(d,i) {
+						return d.height;
+					});	
+				
+			//ranking numbers
+			el.append("text")
+				.attr("font-size",function(d,i) {
+					return d.rankingfontsize;
+				})
+				.attr('y',function(d,i) {
+					return d.rankingnumber_y;
+				})
+				.attr("x",function(d,i) {
+					return d.rankingnumber_x;
+				})
+				.text(function(d,i){
+					return d.rankingnumber;
+				});
+		
+			//dimension name
+			el.append("text")
+				.attr("font-size", function(d,i) {
+					return d.labelfontsize;
+				})
+				.transition()
+				.attr('y',function(d,i) {
+					return d.rankingtext_y;
+				})
+				.attr("x",function(d,i) {
+					return d.rankingtext_x;
+				})
+				.text(function(d,i) {
+					return d.label;
+				});
+		
+			//dimension name underline
+			el.append("line")
+				.attr("x1",function(d,i) {
+					return d.textunderline_start;
+				})
+				.attr("y1",function(d,i) {
+					return d.textunderline_y;
+				})
+				.attr("x2",function(d,i) {
+					return d.textunderline_end;
+				})
+				.attr("y2",function(d,i) {
+					return d.textunderline_y;
+				})
+				.attr("style","stroke:grey;stroke-width:1");	
 			
-			var datapointline = svg.append('g');
-			datapointline.append('line')
-			    .attr('stroke', 'grey')
-				.attr('y1',133)
-				.attr('x1',370)
-				.attr('y2',35)
-				.attr('x2',370)
-			
-			var datapoint = svg.append('g')
-			datapoint.append('circle')
-				.attr('cy', 143)
-				.attr('cx', 370) 
-				.attr('r', 10)
-				.attr('stroke', 'red')
-				.attr('fill', 'white')	
-			datapoint.append('circle')
-				.attr('cy', 143)
-				.attr('cx',370)
-				.attr('r', 7) 
-				.attr('stroke', 'red')
-				.attr('fill', 'white')	
-			datapoint.append('circle')
-				.attr('cy', 143)
-				.attr('cx',370)
-				.attr('r', 4) 
-				.attr('stroke', 'red')
-				.attr('fill', 'white')	
-			datapoint.append('circle')
-				.attr('cy', 143)
-				.attr('cx',370)
-				.attr('r', 1) 
-				.attr('stroke', 'red')
-				.attr('fill', 'white')	
-			datapoint.append('line')
-				.attr('y1',143)
-				.attr('x1',360)
-				.attr('y2',143)
-				.attr('x2',380)
-				.attr('stroke', 'black') 
-			datapoint.append('line')
-				.attr('y1',133)
-				.attr('x1',370)
-				.attr('y2',153)
-				.attr('x2',370)
-				.attr('stroke', 'grey')
-				 
-			var alarmline = svg.append('g');
-			alarmline.append('line')
-			    .attr('stroke', 'grey')
-				.attr('y1',133)
-				.attr('x1',370)
-				.attr('y2',34)
-				.attr('x2',510)										
+			//measurement bars
+			el.append("rect")
+				.attr("x",function(d,i) {
+					return d.measurebar_x;
+				})
+				.attr("y",function(d,i){
+					return d.measurebar_y;
+				})
+				.attr('fill','#80FF80')
+				.attr('stroke', '#80FF00')
+				.attr('width',0)
+				.transition()
+				.duration(500)
+				.attr("width",function(d,i) {
+					return d.measurebar_width;
+				})
+				.attr("height",function(d,i) {
+					return d.measurebar_height;
+				})
+				.attr("style","stroke:grey;stroke-width:1");
+		
+			el.append("text")
+				.attr("font-family","monospace")
+				.attr("font-size",function(d,i) {
+					return d.measurevaluefontsize;
+				})
+				.attr("x",function(d,i) {
+					return d.measurevalue_x;
+				})
+				.attr("y",function(d,i) {
+					return d.measurevalue_y;
+				})
+				.text(function(d,i) {
+					return d.value;
+				});		
 		});		
 	};
 	
 	function drawboxes(rankingbars,data) {
-		el = rankingbars.enter()
-			.append('g');
-		
-		//ranking box
-		el.append("rect")
-			.classed('bar',true)
-			.classed('shadow',true)
-				.attr('fill','#ffffc0')
-				.attr('opacity',.5)
-				.attr('stroke','grey')
-				.style('stroke-width',1)				
-				.attr('x',start_x)
-				.attr('y',function(d,i) {
-					return start_y+(i*boxheight)+(i*boxspacing);
-				})				
-				.attr('width',boxwidth)
-				.attr('height',boxheight);		
-		
-		//ranking number circles
-		el.append("circle").classed('bar',true)
-			.classed('shadow',true)
-				.attr('fill','#ffffc0')
-				.attr('stroke','grey')
-				.style('stroke-width',1)
-				.transition()
-				.attr('cx',circlestart_x)
-				.attr('cy',function(d,i) {
-					return circlestart_y+(i*boxheight)+(i*boxspacing);
-				})
-				.attr('r',numberranksize)
-				.attr('height',boxheight);	
-				
-		//ranking numbers
-		el.append("text")
-			.attr("font-size",numberfontsize)
-			.attr('y',function(d,i) {
-				return numberstart_y+(i*boxheight)+(i*boxspacing);
-			})
-			.attr("x",numberstart_x)
-			.text(function(d,i){return i+1}
-		);
-		
-		//dimention name
-		el.append("text")
-			.attr("font-size",labelfontsize)
-			.transition()
-			.attr('y',function(d,i) {
-				return numberstart_y+(i*boxheight)+(i*boxspacing);
-			})
-			.attr("x",labelxspacing)
-			.text(function(d,i){return d.label}
-		);
-		
-		//dimension name underline
-		el.append("line")
-			.attr("x1",function(d,i) {
-				return labelxspacing;
-			})
-			.attr("y1",function(d,i) {
-				return numberstart_y+(i*boxheight)+(i*boxspacing) + underlineoffsety;
-			})
-			.attr("x2",function(d,i) {
-				return numberstart_x+underlinesize;
-			})
-			.attr("y2",function(d,i) {
-				return numberstart_y+(i*boxheight)+(i*boxspacing) + underlineoffsety;
-			})
-			.attr("style","stroke:grey;stroke-width:1");	
-			
-		//measurement bars
-		el.append("rect")
-			.attr("x",box_x+barmargin)
-			.attr("y",function(d,i){
-				return numberstart_y+(i*boxheight)+(i*boxspacing) + baroffset;
-			})
-			.attr('fill','#80FF80')
-			.attr('stroke', '#80FF00')
-			.attr('width',0)
-			.transition()
-			.duration(500)
-			.attr("width",320)
-			.attr("height",barheight)
-			.attr("style","stroke:grey;stroke-width:1");
-
-		el.append("text")
-			.attr("font-family","monospace")
-			.attr("font-size",valuesfontsize)
-			.attr("x",box_x+barmargin)
-			.attr("y",function(d,i){
-				return numberstart_y+(i*boxheight)+(i*boxspacing) + accumnumberoffset;
-			})
-			.text(function(d,i){
-				return "Accum:" + d.accumvalue;
-			});
-
-		el.append("text")
-			.attr("font-family","monospace")
-			.attr("font-size",valuesfontsize)
-			.attr("x",box_x+barmargin)
-			.attr("y",function(d,i){
-				return numberstart_y+(i*boxheight)+(i*boxspacing) + numberoffset;
-			})
-			.text(function(d,i){
-				return d.value;
-			});
-								
-		//test drilldown lines
-		//el.append("line")
-		//	.attr("x1",function(d,i) {
-		//		return start_x+(boxwidth)
-		//	})
-		//	.attr("y1",function(d,i){
-		//		return start_y + (boxheight*i) + (boxspacing*i) + (boxheight/2)
-		//	})
-		//	.attr("x2",function(d,i){
-		//		return 880;
-		//	})
-		//	.attr("y2",function(d,i){
-		//		return $('#item'+(i+1)).position().top + ($('#item'+(i+1)).height()/2);
-		//	})
-		//	.attr("style","stroke:rgb(255,0,0);stroke-width:2");
-	
 	};
-	
-	function drawrankingboxes() {
-		var datainput = data.data;
-		var total = d3.sum(datainput);
-		var grandtotal = total + data.other;
-		var margin = 20;
-		var boxheight = 60;
-		var boxwidth = 100;
-		var box_x = 100;
-		var numbermargin = 10;
-
-		var colorScale = d3.scale.linear().domain([0,datainput.length]).range(["blue","green"]);
-	
-		//EXPLANATION BOXES
-		var runningy = margin;
-		var lastboxindex = 0;	
-		main.append("rect").attr("y",function(d,i){
-			lastboxindex = i;
-			return (i * boxheight) + margin;
-		}).attr("stroke","black").attr("x", box_x).attr("width",boxwidth).style("fill",function(d,i){return colorScale(i)}).attr("opacity",.5).attr("height",boxheight);
-
-		//NUMBER CIRCLES
-		main.append("circle").attr("cy",function(d,i){
-           return (i * boxheight) + margin + 30;
-		}).attr("cx",150).attr("r",boxheight*.40).attr("fill","yellow").attr("opacity",.8);
-
-		//SEQUENCE NUMBERS
-		main.append("text").attr("font-size","36").attr("y",function(d,i){
-			return (i * boxheight) + margin + 40;
-		}).attr("x",140).text(function(d,i){return i+1});
-
-		//EXPLANATION BOX TOP ITEM DESCRIPTION
-		main.append("text").attr("font-weight","bold").attr("font-size","20").attr("y",function(d,i){
-			return (i * boxheight) + margin+23;
-		}).attr("x",box_x + numbermargin).attr("height",12).text(function(d,i){return d.label;}); //{return datainput.labels[i]}); 	
-
-		//EXPLANATION BOXES CONTENT
-		var runningtotal = 0;
-		dec2 = d3.format(".2f");
-		perc = d3.format(".2%");
-		main.append("text").attr("font-weight","bold").attr("y",function(d,i){
-			return (i * boxheight) + margin + (boxheight-10);
-		}).attr("x",box_x + numbermargin).attr("height",12).text(function(d,i){
-			runningtotal = runningtotal + d.value;
-			return dec2(d) + " (" + perc(d/grandtotal) + ") " + "(Accum: " + perc(runningtotal/grandtotal) + ")";
-		});      
-
-	}
 	
 	return exports;
 };
 
-var topxbase = drillix.analysis.topx();
-var topgraphbase = d3.select("#analysis").datum(data).call(topxbase);   
+var analysisdata = drillix.layout.topx(data);
+var topxbase = drillix.analysis.topx(analysisdata);
+var topgraphbase = d3.select("#analysis").datum(analysisdata).call(topxbase);   
