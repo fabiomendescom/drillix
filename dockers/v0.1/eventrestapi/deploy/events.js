24cd3f9a7779c8d7aeae4283e62d875514487164
Fabio Mendes <fabio@fmendes.net>
2015-05-07 21:20:46 -0700

    working meta

--- dockers/v0.1/eventrestapi/deploy/events.js
+++ dockers/v0.1/eventrestapi/deploy/events.js
@@ -1,12 +1,13 @@
 var express = require('express')
 , passport = require('passport')
 , util = require('util')
-, BasicStrategy = require('passport-http').BasicStrategy;
+, BearerStrategy = require('passport-http-bearer').Strategy;
+var bodyParser = require("body-parser");
+
+
 
 var AWS = require('aws-sdk');
 var http = require('http');
-var seaport = require('seaport');
-var ports = seaport.connect('172.17.42.1', 9090);
 
 function sqsmessageadd(msg, req, res, callback) {
 	var sqs = new AWS.SQS({accessKeyId: req.user.accesskey, secretAccessKey: req.user.secretkey, region: req.user.region});
@@ -24,7 +25,7 @@ function sqsmessageadd(msg, req, res, callback) {
 var users = [
 	{ 
 		id: 1, 
-		username: 'E95C52F99868D96F6791264A1AE4A', 
+		token: 'E95C52F99868D96F6791264A1AE4A', 
 		accesskey: 'AKIAIUAUOG5OVKIGNYWQ', 
 		secretkey: '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',
 		region: 'us-east-1',
@@ -34,44 +35,49 @@ var users = [
 	}
 ];
 
-function findByUsername(username, fn) {
+function findByToken(token, fn) {
 	for (var i = 0, len = users.length; i < len; i++) {
 		var user = users[i];
-		if (user.username === username) {
+		if (user.token === token) {
 			return fn(null, user);
 		}
 	}
 	return fn(null, null);
 }
 
-passport.use(new BasicStrategy({},
-	function(username, password, done) {
-		// Find the user by username. If there is no user with the given
-		// username, or the password is not correct, set the user to `false` to
-		// indicate failure. Otherwise, return the authenticated `user`.
-		findByUsername(username, function(err, user) {
-			if (err) { return done(err); }
-			if (!user) { return done(null, false); }
-			return done(null, user);
-		});
-	}
+passport.use(new BearerStrategy(
+  function(token, done) {
+    findByToken(token, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      return done(null, user, { scope: 'write' });
+    });
+  }
 ));
  
 var app = express();
 
-// Initialize Passport! Note: no need to use session middleware when each
-// request carries authentication credentials, as is the case with HTTP Basic.
 app.use(passport.initialize());
- 
- 
-app.get('/', function(req, res) {	
-	res.send([{name:'wine1'}, {name:'wine2'}]);
-});
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  console.log(req.body) // populated!
+  next()
+})
 
-app.get('/:account/:subaccount/canonical/transactions/:transaction',  passport.authenticate('basic', { session: false }), function(req, res) {
-	var msg = "FAAAAA";
+app.post('/:account/:subaccount/canonical/transactions/:transaction',  passport.authenticate('bearer', { session: false }), function(req, res) {
+	var msg = {productid:111};
 	
-	sqsmessageadd(msg, req, res, function(err, data) {
+	var meta = {};	
+	meta["account"] = req.params.account;
+	meta["subaccount"] = req.params.subaccount;
+	meta["type"] = "T";
+	meta["name"] = req.params.transaction;
+	
+	msg["_drillixmeta"] = meta;
+	
+	var msgtext = JSON.stringify(msg);
+	
+	sqsmessageadd(msgtext, req, res, function(err, data) {
 		if (err) {
 			console.log(err, err.stack); // an error occurred
 			res.status(500);
@@ -79,20 +85,14 @@ app.get('/:account/:subaccount/canonical/transactions/:transaction',  passport.a
 		}
 		else {    
 			console.log(data);           // successful response
-			res.send({"status" : "success"});			
+			res.send({"status" : "success", "id" : data.MessageId, "data" : JSON.stringify(req.body)});			
 			
 		}
 	});
 });
 
-//app.post('/v0.1/events',  passport.authenticate('basic', { session: false }), function(req, res) {
-//	res.send({id:req.params.id, name: "The Name", description: "description"});
-//});
-
-
-var porttolisten = ports.register('web@1.2.3',{port:3000});
+var porttolisten = 3000;
 console.log("preparing to listen on port " + porttolisten);
 app.listen(porttolisten);
-
 console.log("listening on port " + porttolisten);
 
