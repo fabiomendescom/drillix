08969ce5d4583ac9429891a1e8d0f9cd9b2b9029
Fabio Mendes <fabio@fmendes.net>
2015-05-17 22:05:00 -0700

    zookeeper still...

--- dockers/v0.1/eventrestapi/deploy/events.js
+++ dockers/v0.1/eventrestapi/deploy/events.js
@@ -1,12 +1,24 @@
 //INSTANCE SPECIFIC
+var nodename			= process.env.NODE_NAME;
+var porttolisten 		= process.env.PORT;
+var zookeeperurl		= process.env.ZOOKEEPER_URL;
+var queue               = process.env.EVENTQUEUE;
+
 var redisurl			= process.env.REDIS_URL;
 var redisport			= process.env.REDIS_PORT;
-var porttolisten 		= process.env.PORT;
-var queue               = process.env.EVENTQUEUE;
-var seaportaddr			= process.env.SEAPORT_ADDR;
-var seaportport			= process.env.SEAPORT_PORT;
 
 var logger = require('winston');
+var myipaddress;
+var ip = require('local-ip');
+interface = "eth0";
+ip(interface, function(err, res) {
+  if (err) {
+    logger.error('I have no idea what my local ip is.');
+  } else {
+	logger.info('My local ip address on ' + interface + ' is ' + res);
+	myipaddress = res;
+  }
+});
 var express = require('express')
 , passport = require('passport')
 , util = require('util')
@@ -19,7 +31,44 @@ var MongoClient = require('mongodb').MongoClient
 var AWS = require('aws-sdk');
 var http = require('http');
 var redis = require("redis");
-var seaport = require("seaport");
+
+logger.info("Starting registration to zookeeper");
+var zookeeper = require('node-zookeeper-client');
+ 
+var myurl = myipaddress + ":" + porttolisten;
+var client = zookeeper.createClient(zookeeperurl);
+var path = "/DRILLIXSERVICES/EVENTS" + "/" + nodename;
+ 
+client.once('connected', function () {
+    logger.info('Connected to zookeeper service at ' + zookeeperurl);							
+	client.exists(path,function(error, stat) {
+		if(error) {
+			logger.error(error.stack);
+		} else {
+			if(stat) {
+				client.setData(path, new Buffer(myurl), function(error,data,stat) {
+					if(error) {
+						logger.error('Failed to set data on node:' + path + ' due to: ' + error);
+					} else {
+						logger.info("Registered data " + myurl + " on node: " + path);
+					}		
+				})	
+			} else {
+				logger.info("Path " + path + " does not exist. Will create");		
+				client.mkdirp(path, new Buffer(myurl), function (error) {
+					if (error) {
+						logger.error('Failed to create node:' + path + ' due to: ' + error.stack);
+					} else {
+						logger.info("Created path " + path);															
+					}
+				});			
+			}
+		}
+	});
+});
+ 
+client.connect();
+
 
 
 function getSystemInfo() {
@@ -161,13 +210,7 @@ app.post('/:account/:subaccount/canonical/transactions/:transaction',  passport.
 
 });
 
-
-var ports = seaport.connect(seaportaddr, seaportport);
-ports.service('drillixevents@0.0.0', function (port, ready) {
-    log.info("I M HERE");
-});
-
-portchosen = ports.register('drillixevents@0.0.0',{port: porttolisten});
+portchosen = porttolisten;
 logger.info("preparing to listen on port " + portchosen);
 
 app.listen(portchosen);
--- dockers/v0.1/loadbalancerjs/deploy/loadbalancer.js
+++ dockers/v0.1/loadbalancerjs/deploy/loadbalancer.js
@@ -1,37 +1,59 @@
 //INSTANCE SPECIFIC
+var zookeeperurl		= process.env.ZOOKEEPER_URL;
+
 var redisurl			= process.env.REDIS_URL;
 var redisport			= process.env.REDIS_PORT;
-var seaportport			= process.env.SEAPORT_PORT;
 
 var logger = require('winston');
 var http = require('http'),
 httpProxy = require('http-proxy');
-var redis = require("redis");
-var seaport = require('seaport');
-
-
-logger.info("STARTING ZOOKEEPER");
 var zookeeper = require('node-zookeeper-client');
- 
-var client = zookeeper.createClient('172.17.42.1:2181');
-var path = "/testsss";
+var redis = require("redis");
+
+
+
+var client = zookeeper.createClient(zookeeperurl);
+var path = "/DRILLIXSERVICES/REDIS";
  
 client.once('connected', function () {
-    logger.info('Connected to the server.');
- 
-    client.create(path, function (error) {
-        if (error) {
-            logger.error('Failed to create node: %s due to: %s.', path, error);
-        } else {
-            logger.error('Node: %s is successfully created.', path);
-        }
- 
-        client.close();
-    });
+    logger.info('Connected to zookeeper service at ' + zookeeperurl);							
+	client.exists(path,function(error, stat) {
+		if(error) {
+			logger.error(error.stack);
+		} else {
+			if(stat) {
+				console.log("HERE");
+				client.getChildren(path, function (error, children, stats) {
+					if (error) {
+						logger.error(error.stack);
+					} else {
+						console.log("CHILDREN: " + children);
+						var nodetouse = children[0];		
+						client.getData(
+							path + "/" + nodetouse,
+							function (event) {
+								logger.info('Got event: %s.', event);
+							},
+							function (error, data, stat) {
+								if (error) {
+									logger.error(error.stack);
+								} else {
+									logger.info('Got data: %s', data.toString('utf8'));
+								}
+							}
+						);								
+					}
+				});
+			} else {
+				logger.error("Problems");
+			}
+		}
+	});
 });
  
 client.connect();
-logger.info("DONE ZOOKEEPER");
+
+
 
 
 function getSystemInfo() {
@@ -49,10 +71,6 @@ function getSystemInfo() {
 }
 
 logger.info("Starting docker process",getSystemInfo());
-
-var seaportserver = seaport.createServer();
-seaportserver.listen(seaportport);
-logger.info("Seaport listening on " + seaportport);
 
 	var redisclient = redis.createClient(redisport,redisurl);
 
@@ -73,17 +91,6 @@ logger.info("Seaport listening on " + seaportport);
 				mongouri: 'mongodb://heroku_app34960699:pbho09fpelbpp597c21fu0cami@ds029197.mongolab.com:29197/heroku_app34960699'
 		}				
 		redisclient.set(usercontext.token, JSON.stringify(usercontext), redis.print);	
-		var ps = seaportserver.query("drillixevents@0.0.0");
-		if (ps.length === 0) {
-			logger.error('service not available');
-			res.end('service not available\n');
-		}
-		else {
-			logger.info(JSON.stringify(ps));
-			var targeturl = 'http://' + ps[0].host.replace("::ffff:","") + ':' + ps[0].port;
-			logger.info("Proxing to " + targeturl);
-			proxy.web(req, res, { target: targeturl });
-		}		
 	});
 	logger.info("listening on port 80")
 	server.listen(80);
